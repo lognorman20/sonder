79726d96a9c923709b377ef7c7c693f806d8fa09
Logan Norman <lognorman20@gmail.com>
2025-02-27 14:41:44 -0700

    make validator more robust

--- avs/Validation_Service/src/validator.service.js
+++ avs/Validation_Service/src/validator.service.js
@@ -5,12 +5,21 @@ const oracleService = require("./oracle.service");
 async function validate(proofOfTask) {
 
   try {
+    var isApproved = true;
     const taskResult = await dalService.getIPfsTask(proofOfTask);
-    if (Object.keys(taskResult).length > 0) {
-      console.log("Scores is not empty");
-    } else {
+    if (Object.keys(taskResult).length <= 0) {
       console.log("Scores is empty");
+      isApproved = false;
     }
+
+    const isDescending = taskResult.every((_, i, arr) => 
+      i === 0 || arr[i - 1].score >= arr[i].score
+    );
+
+    if (isDescending == false) {
+      isApproved = false;
+    }
+
     return isApproved;
   } catch (err) {
     console.error(err?.message);
