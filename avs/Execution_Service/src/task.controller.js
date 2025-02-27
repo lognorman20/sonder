1ba6e90619ea5f8153ca390aeabbe6a5869138b5
Logan Norman <lognorman20@gmail.com>
2025-02-27 12:15:27 -0700

    score predictions

--- avs/Execution_Service/src/task.controller.js
+++ avs/Execution_Service/src/task.controller.js
@@ -14,16 +14,36 @@ router.post("/execute", async (req, res) => {
         var taskDefinitionId = Number(req.body.taskDefinitionId) || 0;
         console.log(`taskDefinitionId: ${taskDefinitionId}`);
 
-        const result = await oracleService.getPrice("BTCUSDT");
-        result.price = result.price ? result.price : req.body.fakePrice;
-        const cid = await dalService.publishJSONToIpfs(result);
+        // parse out the predictions
+        const predictions = req.body.predictions;
+        const actual = req.body.actual;
+
+        // score predictions
+        const scores = [];
+        for (const pred of predictions) {
+            console.log("pred:\n" + pred);
+            const text = pred.text;
+            const score = await oracleService.score(text, actual);
+            scores.push({ score: score, creator: pred.agent_creator });
+        }
+
+        // rank agents
+
+        // send rankings to smart contract
+
+        // send rankings to api
+
+        // post predictions to eigenda
         // const [cid, poll] = await dalService.publishToEigenDA(result);
-        const data = result.price;
-        await dalService.sendTask(cid, data, taskDefinitionId);
-        return res.status(200).send(new CustomResponse({proofOfTask: cid, data: data, taskDefinitionId: taskDefinitionId}, "Task executed successfully"));
         // const blob = await poll;
         // console.log(`blob data: ${blob}`);
         // await dalService.sendTask(cid, data, taskDefinitionId);
+
+        // normal flow (remove iff eigenda being used)
+        const cid = await dalService.publishJSONToIpfs(scores);
+        // const data = "hello";
+        await dalService.sendTask(cid, scores.join(" "), taskDefinitionId);
+        return res.status(200).send(new CustomResponse({proofOfTask: cid, data: scores, taskDefinitionId: taskDefinitionId}, "Task executed successfully"));
     } catch (error) {
         console.log(error)
         return res.status(500).send(new CustomError("Something went wrong", {}));
